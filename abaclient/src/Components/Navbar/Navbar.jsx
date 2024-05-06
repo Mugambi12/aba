49a4aa801c58a65374bf6169d89b8e53acce186b
mugambi12 <silasmungiria16@gmail.com>
2024-05-06 22:08:28 +0300

    chore: Update Navbar styling and functionality

--- abaclient/src/Components/Navbar/Navbar.jsx
+++ abaclient/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,34 @@
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="nav">
+      <div className="nav-logo">ABA</div>
+      <ul className="nav-menu">
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Explore</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Services</a>
+        </li>
+        <li>
+          <a href="">Projects</a>
+        </li>
+        <li>
+          <a href="">Blog</a>
+        </li>
+        <li className="nav-contact">
+          <a href="">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
