48a56122de1b5098a76c9d2d46a967ad4b7c67fc
mugambi12 <silasmungiria16@gmail.com>
2024-05-06 22:08:20 +0300

    chore: Update Hero component styling and functionality

--- abaclient/src/Components/Hero/Hero.jsx
+++ abaclient/src/Components/Hero/Hero.jsx
@@ -1,19 +1,51 @@
 import "./Hero.css";
+import arrow_btn from "../../assets/arrow_btn.png";
+import play_icon from "../../assets/play_icon.png";
+import pause_icon from "../../assets/pause_icon.png";
 
-const Hero = () => {
-  return (
-    <div>
-      Hero
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
-        sequi obcaecati reiciendis ea ad sit culpa accusamus tempore quibusdam,
-        iusto praesentium, temporibus nesciunt qui est necessitatibus cum, illum
-        tenetur! Totam, hic deserunt excepturi similique facilis eum velit nobis
-        pariatur quis ullam aperiam rerum esse ut? Eaque quibusdam alias
-        officiis temporibus!
-      </p>
+const Hero = ({
+  heroData,
+  heroCount,
+  setHeroCount,
+  setPlayStatus,
+  playStatus,
+}) => (
+  <div className="hero">
+    <div className="hero-text">
+      <p className="hero-title">{heroData.title}</p>
+      <p className="hero-subtitle">{heroData.subtitle}</p>
     </div>
-  );
-};
+
+    <div
+      className="hero-explore"
+      onClick={() => setHeroCount((prevCount) => (prevCount + 1) % 3)}
+    >
+      <p>Explore the features</p>
+      <img src={arrow_btn} alt="Arrow button" />
+    </div>
+
+    <div className="hero-dot-play">
+      <ul className="hero-dots">
+        {Array.from({ length: 3 }, (_, index) => (
+          <li
+            key={index}
+            onClick={() => setHeroCount(index)}
+            className={`hero-dot ${heroCount === index ? "orange" : ""}`}
+          />
+        ))}
+      </ul>
+
+      <div className="hero-play">
+        <img
+          src={playStatus ? pause_icon : play_icon}
+          alt={playStatus ? "Pause icon" : "Play icon"}
+          onClick={() => setPlayStatus(!playStatus)}
+          aria-label={playStatus ? "Pause video" : "Play video"}
+        />
+        <p>See the video</p>
+      </div>
+    </div>
+  </div>
+);
 
 export default Hero;
