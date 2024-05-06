e2060437734cccf1a4fc073038ed0255b5c2658b
mugambi12 <silasmungiria16@gmail.com>
2024-05-06 18:43:32 +0300

    feat: Add lazy loading for background images

--- abaclient/src/App.jsx
+++ abaclient/src/App.jsx
@@ -1,21 +1,19 @@
-import React from "react";
+import { useState } from "react";
+import Background from "./Components/Background/Background";
 
 const App = () => {
+  let heroData = [
+    { text1: "Welcome to", text2: "ABA" },
+    { text1: "Client Portal", text2: "We are here to help you" },
+    { text1: "Biodiversity", text2: "We are here to help you" },
+  ];
+
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
+
   return (
     <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel possimus
-        accusamus quis reprehenderit omnis quaerat, expedita natus laboriosam
-        tempore laborum est aspernatur odio iusto, nemo eos nihil recusandae
-        fugit! Neque a asperiores aspernatur ipsa ex ipsum recusandae minus
-        tempore repudiandae expedita. Accusantium eos unde ea aliquid deserunt,
-        doloremque dolores inventore vitae! Consequatur cupiditate nihil ratione
-        minus possimus corrupti quaerat assumenda culpa nesciunt id laborum
-        consectetur sapiente fugiat molestiae natus obcaecati quo quis dolor
-        nulla temporibus, eum veritatis facilis? Ex aperiam ullam itaque
-        adipisci suscipit. Ad, dolor hic possimus recusandae consequatur dolorem
-        facere, ipsum asperiores sint aliquid, libero blanditiis nesciunt saepe.
-      </p>
+      <Background playStatus={playStatus} heroCount={heroCount} />
     </div>
   );
 };
---
+++ abaclient/src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div>
+      Hero
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+        sequi obcaecati reiciendis ea ad sit culpa accusamus tempore quibusdam,
+        iusto praesentium, temporibus nesciunt qui est necessitatibus cum, illum
+        tenetur! Totam, hic deserunt excepturi similique facilis eum velit nobis
+        pariatur quis ullam aperiam rerum esse ut? Eaque quibusdam alias
+        officiis temporibus!
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
