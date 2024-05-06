b4167b12888671be51f4034c6cf3efac22869576
mugambi12 <silasmungiria16@gmail.com>
2024-05-06 22:07:41 +0300

    refactor: Update App component to include Navbar and Hero components

--- abaclient/src/App.jsx
+++ abaclient/src/App.jsx
@@ -1,19 +1,38 @@
 import { useState } from "react";
 import Background from "./Components/Background/Background";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 const App = () => {
   let heroData = [
-    { text1: "Welcome to", text2: "ABA" },
-    { text1: "Client Portal", text2: "We are here to help you" },
-    { text1: "Biodiversity", text2: "We are here to help you" },
+    {
+      title: "Welcome to ABA",
+      subtitle: "Your trusted partner in innovative solutions",
+    },
+    {
+      title: "Client Portal",
+      subtitle: "Access your account and manage services with ease",
+    },
+    {
+      title: "Biodiversity",
+      subtitle: "Discover our commitment to preserving the environment",
+    },
   ];
 
-  const [heroCount, setHeroCount] = useState(2);
+  const [heroCount, setHeroCount] = useState(0);
   const [playStatus, setPlayStatus] = useState(false);
 
   return (
     <div>
       <Background playStatus={playStatus} heroCount={heroCount} />
+      <Navbar />
+      <Hero
+        setHeroCount={setHeroCount}
+        heroCount={heroCount}
+        setPlayStatus={setPlayStatus}
+        playStatus={playStatus}
+        heroData={heroData[heroCount]}
+      />
     </div>
   );
 };
